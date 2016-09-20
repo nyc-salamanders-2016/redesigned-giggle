2d66777c35a10bb26d72ffe2219671950ea75ba0
raemilla <rachael.miller05@gmail.com>
2016-09-20 16:25:13 -0400

    phase 0 working without number of student pitches constraints

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy'
   post '/flows/new', to: 'pages#create'
 
+
   root 'pages#home'
 
   resources :students, only: [:new, :create]
+  resources :pitches
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
